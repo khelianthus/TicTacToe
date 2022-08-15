b7e67f130dc6b4ec9d32930a3193a852d8a0a259
khelianthus <x@x.com>
2022-08-15 11:52:18 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TicTacToe {
+
+    public class Program
+    {
+        public static void Main(string[ args]){
+
+        //Create array for the playingfield consisting of 9 places
+        string[] ticTacToeFieldArray = new string[9] {"-","-","-","-","-","-","-","-","-"};
+        string playfield = "";
+        //Store players score.
+        int playerXScore = 0;
+        int playerOScore = 0;
+        //Store player token, choice of token
+        string? playerX = "X";
+        string? playerO = "O";
+
+        //Start.newGameStart();
+
+        //Need to use for ReadKey to work properly, and a name for variable
+        ConsoleKeyInfo userChoice;
+        Console.WriteLine("Welcome to a game of Tic-Tac-Toe!\n");
+        Console.ReadLine();
+        Console.WriteLine("What is your name?: ");
+        string? playerName = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine($"Will {playerName} be X or O?:");
+        //insert the choice in variable.
+        userChoice = Console.ReadKey();
+    
+
+        if (userChoice.Key == ConsoleKey.X){
+            Console.WriteLine("You chose X!");
+            playerX = userChoice.ToString();
+        }
+        else if (userChoice.Key == ConsoleKey.O){
+            Console.WriteLine("You chose O!");
+            playerO = userChoice.ToString();
+        }
+        else {
+            Console.WriteLine("Please select between X or O.");
+        }
+
+        Console.Clear();
+
+        //Call board function.
+        Board();
+
+    public static void Board()
+    {
+       Console.WriteLine("     |     |      ");
+       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[1], ticTacToeFieldArray[2], ticTacToeFieldArray[3]);
+       Console.WriteLine("_____|_____|_____ ");
+       Console.WriteLine("     |     |      ");
+       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[4], ticTacToeFieldArray[5], ticTacToeFieldArray[6]);
+       Console.WriteLine("_____|_____|_____ ");
+       Console.WriteLine("     |     |      ");
+       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[7], ticTacToeFieldArray[8], ticTacToeFieldArray[9]);
+       Console.WriteLine("     |     |      ");
+    }
+
+           }
+        }
+    }
---
+++ README.txt
@@ -0,0 +1,35 @@
+# TIC-TAC-TOE
+
+[] create playing field, position 0-8
+[] randomize who starts to play
+[x] choose if you're "x" or "o" 
+[] create 9 spots to fill
+[] check if each spot is empty before filling it in
+[] check if there's a win after a win-able connection has been formed
+[] give win to winner
+[] best of 3?
+[] decide who winner and looser is or if there's a tie
+
+# VARIABLES
+[x] player, computer
+[x] player score, comptuer score
+[] games played
+[] array for playing field
+[] possibly adding player 2?
+
+# METHODS/FUNCTIONS
+[] players selection
+[] computer selection
+[] game conclussion check
+	[] all these in one method called "game".
+
+# LOOPS
+[] game loop through players selection and computer selection for 3 games.
+[] ask play again to run it again, saves score
+
+# DISPLAY
+[] playing field in console
+
+# PLAYER INTERACTION
+[] being able to move by keyboard on the field
+[] console not accepting other user inputs than the x or o, arrow key moves, esc and enter.
