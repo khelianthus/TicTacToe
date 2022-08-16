1026e5e46c73b104e64cb6bca9744bd8e43f947d
khelianthus <x@x.com>
2022-08-16 18:45:49 +0200

    Cleaned up Board function some more, working a bit on Game function logics

--- Program.cs
+++ Program.cs
@@ -15,10 +15,22 @@ namespace TicTacToe {
         string? playerX = "X";
         string? playerO = "O";
 
+        do {
+
         //Call start function.
         Start();
         //Call board function.
         Board();
+        //Game();
+
+
+
+        Console.Clear();
+
+        } while (true){ 
+
+    }
+
 
     void Start(){
 
@@ -26,40 +38,72 @@ namespace TicTacToe {
         ConsoleKeyInfo userChoice;
         Console.WriteLine("Welcome to a game of Tic-Tac-Toe!\n");
         Console.ReadLine();
-        Console.WriteLine("What is your name?: ");
+        Console.WriteLine("Who is the first player?: ");
         //Need questionmark because input can be null but should be sting
-        string? playerName = Console.ReadLine();
+        string? playerName1 = Console.ReadLine();
         Console.Clear();
-        Console.WriteLine($"Will {playerName} be X or O?:");
+        Console.WriteLine($"Will {playerName1} be X or O?:\n");
         //insert the choice in variable.
         userChoice = Console.ReadKey();
 
         if (userChoice.Key == ConsoleKey.X){
-            Console.WriteLine("You chose X!");
+            Console.WriteLine($"{playerName1} chose X!");
             playerX = userChoice.ToString();
         }
         else if (userChoice.Key == ConsoleKey.O){
-            Console.WriteLine("You chose O!");
+            Console.WriteLine($"{playerName1} chose O!");
             playerO = userChoice.ToString();
         }
         else {
             Console.WriteLine("Please select between X or O.");
         }
         Console.Clear();
+
+        Console.WriteLine("Who is the second player?:");
+        string? playerName2 = Console.ReadLine();
+
+        if (userChoice.Key == ConsoleKey.X){
+            Console.WriteLine($"{playerName2} is O.");
+            playerO = userChoice.ToString();
+            Console.WriteLine(playerO); 
+        }
+
+        else {
+            Console.WriteLine($"{playerName2} is X.");
+            playerX = userChoice.ToString();
+            Console.WriteLine(playerX);
+
+        }
+    
 }
 
     void Board(){
        Console.WriteLine("     |     |      ");
-       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[0], ticTacToeFieldArray[1], ticTacToeFieldArray[2]);
+       Console.WriteLine($"{ticTacToeFieldArray[0]} | {ticTacToeFieldArray[1]} | {ticTacToeFieldArray[2]}");
        Console.WriteLine("_____|_____|_____ ");
        Console.WriteLine("     |     |      ");
-       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[3], ticTacToeFieldArray[4], ticTacToeFieldArray[5]);
+       Console.WriteLine($"{ticTacToeFieldArray[3]} | {ticTacToeFieldArray[4]} | {ticTacToeFieldArray[5]}");
        Console.WriteLine("_____|_____|_____ ");
        Console.WriteLine("     |     |      ");
-       Console.WriteLine("  {0}  |  {1}  |  {2}", ticTacToeFieldArray[6], ticTacToeFieldArray[7], ticTacToeFieldArray[8]);
+       Console.WriteLine($"{ticTacToeFieldArray[6]} | {ticTacToeFieldArray[7]} | {ticTacToeFieldArray[8]}");
        Console.WriteLine("     |     |      ");
     }
 
+    static void Game(){
+    //int containing choice or where to put token, transform string to int
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+        while (true){
+
+            if (ticTacToeFieldArray[choice] =! "-"){
+            playerSelection = 1;
+        }
+
+        break;
+    
+    }
+
+    }
            }
         }
     }
