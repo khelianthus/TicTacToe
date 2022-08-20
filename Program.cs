831f3b585f391899fd7f6ab35a3c9aa30297dc66
khelianthus <x@x.com>
2022-08-20 13:42:50 +0200

    Working on menu function.

--- Program.cs
+++ Program.cs
@@ -7,70 +7,78 @@ namespace TicTacToe {
 
         //Create array for the playingfield consisting of 9 places, using char because we only use one character at the time in the array
         char[] playField = new char[9] {'-','-','-','-','-','-','-','-','-'};
-        //string playfield = "";
-        //Store players score.
-        //int playerXScore = 0;
-        //int playerOScore = 0;
         //Store player token, choice of token
-        string? playerX = "X";
-        string? playerO = "O";
+        string? playerX = "";
+        string? playerO = "";
+        //Stores the players names
+        string playerName1 ="";
+        string playerName2= "";
+        //Saves who's turn it is
         int currentPlayer = 0;
 
         //Call start function.
         Start(playerX, playerO);
+        StartPlayerChoice(currentPlayer, playerName1, playerName2);
         //Call board function, passing through char array through function
-        Board(playField, Start().playerName1, Start().playerName2);
+        Board(playField, currentPlayer);
         //Game();
-        Console.Clear();
 
     void Start(string playerX, string playerO){
 
         //Need to use for ReadKey to work properly, and a name for variable
         ConsoleKeyInfo userChoice = new ConsoleKeyInfo();
         Console.WriteLine("Welcome to a game of Tic-Tac-Toe!\n");
-        Console.WriteLine();
+        Console.ReadLine();
         Console.WriteLine("Who is the first player?: ");
         //Need questionmark after datatype because input can be null but should be sting
         string? playerName1 = Console.ReadLine();
-        Console.WriteLine($"Will {playerName1} be X or O?:\n");
+        Console.Write($"Will {playerName1} be X or O?:\n");
         //insert the choice in variable.
         userChoice = Console.ReadKey();
 
         //Using KeyChar to transfer the pressed key to char, then string.
         if (userChoice.Key == ConsoleKey.X){
             Console.WriteLine($"{playerName1} chose X!");
-            Console.ReadLine();
             playerX = userChoice.KeyChar.ToString();
+            Console.WriteLine(playerX);
+            Console.Read();
         }
+
         else if (userChoice.Key == ConsoleKey.O){
             Console.WriteLine($"{playerName1} chose O!");
-            Console.ReadLine();
             playerO = userChoice.KeyChar.ToString();
+            Console.WriteLine(playerO);
+            Console.Read();
         }
+
         else {
             Console.WriteLine("Please select between X or O.");
-            Console.ReadLine();
+            Console.Read();
         }
         Console.Clear();
 
         Console.WriteLine("Who is the second player?:");
         string? playerName2 = Console.ReadLine();
 
+    //If first user chose X, second player is given O
         if (userChoice.Key == ConsoleKey.X){
             Console.WriteLine($"{playerName2} is O.");
-            playerO = userChoice.KeyChar.ToString();
+            playerO = "O";
             Console.WriteLine(playerO);
-            Console.ReadLine();
+            Console.Read();
         }
 
-        else {
+        //If first user chose O, second player is given X
+        else{
             Console.WriteLine($"{playerName2} is X.");
-            playerX = userChoice.KeyChar.ToString();
+            playerX = "X";
             Console.WriteLine(playerX);
-            Console.ReadLine();
+            Console.Read();
         }
-    
-    Console.Clear();
+        Console.Clear();
+    }
+
+    void StartPlayerChoice (int currentPlayer, string playerName1, string playerName2){
 
     Console.WriteLine("Who will start?");
     Console.ReadLine();
@@ -80,53 +88,61 @@ namespace TicTacToe {
     //Saves where the player is on the menu
     int playerMenuSelection = 0;
 
+    while (true) {
+    Console.Clear();
+    Console.CursorVisible = false;
+
     if (playerMenuSelection == 0){
         Console.WriteLine(playersPlaying[0] + "←");
         Console.WriteLine(playersPlaying[1]);
-        Console.ReadLine();
     }
 
     else if (playerMenuSelection == 1){
         Console.WriteLine(playersPlaying[0]);
         Console.WriteLine(playersPlaying[1] + "←"); 
-        Console.ReadLine();
-
     }
 
     var keyPressed = Console.ReadKey();
-    if (keyPressed.Key == ConsoleKey.RightArrow && playerMenuSelection != playersPlaying.Length -1){
+    if (keyPressed.Key == ConsoleKey.DownArrow && playerMenuSelection != playersPlaying.Length -1){
         playerMenuSelection++;
     }
-    else if (keyPressed.Key == ConsoleKey.LeftArrow && playerMenuSelection >= 1){
+    else if (keyPressed.Key == ConsoleKey.UpArrow && playerMenuSelection >= 1){
         playerMenuSelection--;
     }
 
+    else if (keyPressed.Key == ConsoleKey.Enter){
+
     switch (playerMenuSelection){
         case 0:
+        Console.WriteLine($"{playerName1} will start.");
+        Console.Read();
         currentPlayer = 1;
-        break;
+        return;
 
         case 1:
+        Console.WriteLine($"{playerName2} will start.");
+        Console.Read();
         currentPlayer = 2;
-        break;
+        return;
+            }
+        }
     }
 
-    Console.ReadLine();
-
+      }
 }
-
-    void Board(char[] playField, string playerName1, string playerName2){
-        Console.WriteLine($"{playerName1}'s score:");
-        Console.WriteLine($"{playerName2}'s score:");
+    //string playerName1, string playerName2
+    private static void Board(char[] playField, int currentPlayer){
+        /*Console.WriteLine($"{playerName1}'s score:");
+        Console.WriteLine($"{playerName2}'s score:");*/
 
         Console.WriteLine("     |     |      ");
-        Console.WriteLine($"{playField[0]}  |  {playField[1]}  |  {playField[2]}");
+        Console.WriteLine($"  {playField[0]}  |  {playField[1]}  |  {playField[2]}");
         Console.WriteLine("_____|_____|_____ ");
         Console.WriteLine("     |     |      ");
-        Console.WriteLine($"{playField[3]}  |  {playField[4]}  |  {playField[5]}");
+        Console.WriteLine($"  {playField[3]}  |  {playField[4]}  |  {playField[5]}");
         Console.WriteLine("_____|_____|_____ ");
         Console.WriteLine("     |     |      ");
-        Console.WriteLine($"{playField[6]}  |  {playField[7]}  |  {playField[8]}");
+        Console.WriteLine($"  {playField[6]}  |  {playField[7]}  |  {playField[8]}");
         Console.WriteLine("     |     |      ");
 
         Console.WriteLine($"It's {currentPlayer}'s turn!");
@@ -150,4 +166,3 @@ namespace TicTacToe {
         
         }
     }
-}
